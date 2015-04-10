dda50b3b923c56427b1ed9f9eed92d744c17e632
Andrew Carlson <acarl005@g.ucla.edu>
2015-04-09 21:46:28 -0700

    works for everything except overlap

--- source/boggle_board.rb
+++ source/boggle_board.rb
@@ -1,15 +1,4 @@
-=begin
-1) Select first element, within nested array.
-2) Call adjacent on this element. As the argument for adjacent
-put in every one of the elements on the next nested array.
-3)
-
-
-=end
-
-
-
-TEST_BOARD = [['E', 'T', 'A', 'A'], ['A', 'I', 'E', 'T'], ['R', 'L', 'T', 'N'], ['I', 'A', 'T', 'O']]
+TEST_BOARD = [['E', 'T', 'A', 'Q'], ['A', 'I', 'E', 'T'], ['R', 'L', 'T', 'N'], ['I', 'A', 'T', 'O']]
 
 class DiceGroup
   attr_reader :dice
@@ -46,21 +35,38 @@ class Coordinate
     @x = x
     @y = y
     @used = false
-    @next = nil
   end
+
   def to_s
     "(#{x}, #{y})"
   end
+
+  def same_as?(coord)
+    (self.x - coord.x) == 0 && (self.y - coord.y) == 0
+  end
+
   def adjacent?(coord)
+    return false if self.same_as?(coord)
     (self.x - coord.x).abs <= 1 && (self.y - coord.y).abs <= 1
   end
+
+  def adjacents
+    all = [0, 1, 2, 3].permutation(2).map { |pair| Coordinate.new(pair[0], pair[1]) }
+    (0..3).each { |i| all << Coordinate.new(i, i) }
+    all.select! { |coord1| coord1.adjacent?(self) }
+    # returns array of adjacent coordinates
+  end
+
 end
 
 class BoggleBoard
   attr_reader :board
   def initialize
+    @board = Array.new(4) { (0..3).map { '_' } }
+  end
+
+  def test
     @board = TEST_BOARD
-    # Array.new(4) { (0..3).map { '_' } }
   end
 
   def shake!
@@ -78,34 +84,23 @@ class BoggleBoard
   end
 
   def include?(word)
-    word.upcase!
-    nested_array = []
-    word.each_char { |c|
-      nested_array << get_coords(c)
-    }
-    find_path(nested_array)
-    found = false
-    nested_array[0].each { |coord|
-      node = coord
-      (word.length - 1).times {
-        node = node.next if node
-      }
-      found ||= !!node
-    }
-    found
+    word.upcase!.gsub!(/QU/, 'Q')
+    start_points = get_coords(word.slice!(0))
+    path_found = false
+    start_points.each { |start| path_found ||= find_path(start, word) }
+    path_found
   end
 
-  def find_path(array)
-    array.each_with_index do |sub_array, i|
-      sub_array.each do |coord1|
-        break if i == array.length - 1
-        array[i+1].each do |coord2|
-          if coord1.adjacent?(coord2)
-            coord1.next = coord2
-          end
-        end
+  def find_path(coord, wordpath)
+    return true if wordpath == ""
+    adjacents = coord.adjacents
+    path_found = false
+    adjacents.each { |new_coord|
+      if self.check_this_coord(new_coord) == wordpath[0]
+        path_found ||= find_path(new_coord, wordpath.slice(1, wordpath.length))
       end
-    end
+    }
+    path_found
   end
 
   def get_coords(let)
@@ -120,16 +115,20 @@ class BoggleBoard
     coords
   end
 
+  def check_this_coord(coord)
+    board[coord.y][coord.x]
+  end
+
 end
 
 game = BoggleBoard.new
 dice = DiceGroup.new
 puts game
+game.test
+puts game
 
 coord1 = Coordinate.new(0,2)
 coord2 = Coordinate.new(0,1)
-puts game.include?('ALI')
-puts game.include?('alton')
-puts game.include?('lirt') == false
-
+%w{aietno earil rlto quat ttie}.each { |word| p game.include?(word) }
+%w{lro eitr non}.each { |word| p game.include?(word) == false }
 
